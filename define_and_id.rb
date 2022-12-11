800d8d9a22c8fcb25969f9acc578c75a70ab50b5
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2022-12-11 16:27:14 -0700

    Define methods

--- define_and_id.rb
+++ define_and_id.rb
@@ -11,17 +11,28 @@
 
 
 "Hello World".include?("Hello")
-
+#the include? method is called on the string object "Hello World"
+#The argument "Hello" is passed; include? checks to see if the string object "Hello World" includes the string "Hello."
+#The return value is true
 
 "Hello World".end_with?("Hello")
-
+#The end_with? method is called on the string object "Hello World"
+#The argument "Hello" is passed; end_with? checks to see if the string object "Hello World" ends with the string "Hello."
+#The return value is false
 
 "Hello World".end_with?("rld")
-
+#The end_with? method is called on the string object "Hello World"
+#The argument "rld" is passed; end_with? checks to see if the string object "Hello World" ends with the string "rld."
+#The return value is true.
 
 12.even?
-
+#The even? method is called on the integer 12
+#No argument is passed; even? checks to see if the integer 12 is an even number. 
+#The return value is true.
 
 18.next
+#The next method is called on the integer 18
+#No argument is passed; next prints n+1 where n=an integer object. 
+#The return value is 19.
 
 
